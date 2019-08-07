1637f5cce844e20d665e76e14951c11dab18f9f7
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-08-07 15:02:18 -0400

    added the functionality to delete a todo

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
-const Todo = props => <li>{props.text}</li>
+const Todo = props => {
+    return (
+        <div>
+            <li>
+            <span>{props.todo.text}</span><button onClick={() => props.deleteTodo(props.todo.id)} >Delete</button>
+            </li>
+        </div>
+    )
+}
 
 export default Todo;
